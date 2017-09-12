936ee83ae0005cb1449f425e72d2e66617c91701
Gaál Gergő <gergo0122@gmail.com>
2017-09-12 23:19:14 +0200

    #2 task

--- work-scheduler-web/src/main/java/com/github/gaalgergo/workscheduler/web/controllers/RegistrationController.java
+++ work-scheduler-web/src/main/java/com/github/gaalgergo/workscheduler/web/controllers/RegistrationController.java
@@ -1,18 +1,31 @@
 package com.github.gaalgergo.workscheduler.web.controllers;
 
+import com.github.gaalgergo.workscheduler.service.domain.Employee;
+import com.github.gaalgergo.workscheduler.service.employees.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class RegistrationController {
 
+    @Autowired
+    private EmployeeService employeeService;
+
     @GetMapping("/pages/registration")
-    public String registration() {
+    public String registration(final Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+
         return "registration";
     }
-    @PostMapping("registration")
-    public void registrationSubmit() {
-        System.out.println("done");
+    @PostMapping("/registration")
+    public void registrationSubmit(final Employee employee) {
+        employeeService.save(employee);
+        System.out.println("Done");
     }
 }
